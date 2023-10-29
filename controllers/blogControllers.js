cb4e707cea11f6cb222f196e789f00677a8404a6
myself-karthick <kartthickmohan@gmail.com>
2023-10-29 17:52:29 +0530

    Handled errors

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import lodash from "lodash";
 import memoize from "lodash.memoize";
 const cacheDuration = 60000;
+
 const memoizedFetchBlogs = memoize(
   async () => {
     try {
@@ -28,26 +29,31 @@ const getBlogStats = async (req, res) => {
     const blogs = await memoizedFetchBlogs();
 
     if (blogs.status === 404) {
-      throw { status: blogs.status, message: "Data not fetched" };
+      throw { status: blogs.status, message: "Error in fetching data" };
     }
 
     const totalBlogs = blogs.length;
     const search_string = "privacy";
-    const longestTitleBlog = lodash.maxBy(blogs, (blog) => blog.title.length);
+    const longestTitleBlog =
+      totalBlogs > 0 ? lodash.maxBy(blogs, (blog) => blog.title.length) : null;
 
-    const privacyBlogs = lodash.filter(blogs, (blog) =>
-      blog.title.toLowerCase().includes(search_string)
-    );
+    const privacyBlogs =
+      totalBlogs > 0
+        ? lodash.filter(blogs, (blog) =>
+            blog.title.toLowerCase().includes(search_string)
+          )
+        : [];
 
-    const uniqueTitles = lodash
-      .uniqBy(blogs, "title")
-      .map((blog) => blog.title);
+    const uniqueTitles =
+      totalBlogs > 0
+        ? lodash.uniqBy(blogs, "title").map((blog) => blog.title)
+        : [];
 
     res.json({
       "Total no. of blogs": totalBlogs,
-      "Longest title": longestTitleBlog.title,
-      "No. of blogs with private in title": privacyBlogs.length,
-      "No. of Unique titles": uniqueTitles,
+      "Longest title": longestTitleBlog ? longestTitleBlog.title : "No blogs found",
+      "No. of blogs with 'privacy' in title": privacyBlogs.length,
+      "List of Unique titles": {Total: uniqueTitles.length, Titles: uniqueTitles},
     });
   } catch (error) {
     let message = error;
@@ -61,23 +67,33 @@ const getBlogStats = async (req, res) => {
 const blogSearch = async (req, res) => {
   try {
     if (!req.query.query) {
-      throw "Value in query parameter is required.";
+      throw "A value in the query parameter is required.";
     }
 
     const query = req.query.query.toLowerCase();
     const blogs = await memoizedFetchBlogs();
 
     if (blogs.status === 404) {
-      throw { message: { status: blogs.status, message: "Data not fetched" } };
+      throw { status: blogs.status, message: "Error in fetching data" };
     }
 
-    const privacyBlogs = lodash.filter(blogs, (blog) =>
+    const filteredBlogs = lodash.filter(blogs, (blog) =>
       blog.title.toLowerCase().includes(query)
     );
 
-    res.json(privacyBlogs);
+    if (!filteredBlogs.length) {
+      res
+        .status(404)
+        .json({ message: "No blogs were found for the searched word." });
+    } else {
+      res.json(filteredBlogs);
+    }
   } catch (error) {
-    res.status(500).json({ error });
+    let message = error;
+    if (error.message) {
+      message = error.message;
+    }
+    res.status(error.status || 500).json({ error: message });
   }
 };
 
